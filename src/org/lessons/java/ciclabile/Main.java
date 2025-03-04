a1988b648eeedb398275ce8aee9c04f0270f137f
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-04 16:08:47 +0100

    first commit

---
+++ src/org/lessons/java/ciclabile/Main.java
@@ -0,0 +1,17 @@
+package org.lessons.java.ciclabile;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        Number firstArray = new Number(arr);
+        firstArray.getNumbers();
+        firstArray.getNumbers();
+        firstArray.getNumbers();
+        
+        firstArray.hasAncoraElementi();
+
+        firstArray.addElemento(10);
+
+    }
+}
---
+++ src/org/lessons/java/ciclabile/Number.java
@@ -0,0 +1,39 @@
+package org.lessons.java.ciclabile;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Number {
+    private int[] numbers;
+    HashSet<Integer> num = new HashSet<>();
+    Iterator<Integer> iterator;
+    Integer currentNumber;
+
+    // * costruttore
+    public Number(int[] numbers) {
+        this.numbers = numbers;
+
+        for (int i = 0; i < numbers.length; i++) {
+            num.add(numbers[i]);
+        }
+
+        this.iterator = num.iterator();
+    }
+
+    
+    public void getNumbers() {
+        currentNumber = iterator.next();
+        System.out.println(currentNumber);
+    }
+
+    public void hasAncoraElementi() {
+        System.out.println("Sono presenti altri numeri oltre a questo: " + iterator.hasNext());
+    }
+
+    public void addElemento(int numbetToAdd) {
+        num.add(numbetToAdd);
+        System.out.println("è stato inserito il numero: " + numbetToAdd);
+        System.out.println(num);
+    }
+
+}
