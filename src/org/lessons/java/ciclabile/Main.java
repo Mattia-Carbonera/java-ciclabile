46910b96483dcf57499a4825b6f3cd54f95783b1
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-05 13:28:06 +0100

    fix Main example

--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -2,7 +2,7 @@ package org.lessons.java.ciclabile;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 5, 6, 7, 8, 9};
 
         Number firstArray = new Number(arr);
         firstArray.getNumbers();
@@ -11,6 +11,8 @@ public class Main {
         
         firstArray.hasAncoraElementi();
 
+        firstArray.addElemento(4);
+        firstArray.addElemento(11);
         firstArray.addElemento(10);
 
     }
