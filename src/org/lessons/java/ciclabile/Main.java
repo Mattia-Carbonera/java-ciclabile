ef05f5add84b4cad491dd8e2580398891e8bd6bc
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-05 14:22:02 +0100

    Add feature: added upgrade for numbers array at num updating

--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -2,7 +2,7 @@ package org.lessons.java.ciclabile;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         Number firstArray = new Number(arr);
         firstArray.getNumbers();
@@ -11,9 +11,11 @@ public class Main {
         
         firstArray.hasAncoraElementi();
 
-        firstArray.addElemento(4);
-        firstArray.addElemento(11);
         firstArray.addElemento(10);
+        firstArray.addElemento(11);
+        firstArray.addElemento(12);
+
+        firstArray.debug();
 
     }
 }
--- src/org/lessons/java/ciclabile/Number.java
+++ src/org/lessons/java/ciclabile/Number.java
@@ -1,11 +1,12 @@
 package org.lessons.java.ciclabile;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
 public class Number {
     private int[] numbers;
-    private HashSet<Integer> num = new HashSet<>();
+    private ArrayList<Integer> num = new ArrayList<>();
     private Iterator<Integer> iterator;
     private Integer currentNumber;
 
@@ -30,10 +31,30 @@ public class Number {
         System.out.println("Sono presenti altri numeri oltre a questo: " + iterator.hasNext());
     }
 
-    public void addElemento(int numbetToAdd) {
-        num.add(numbetToAdd);
-        System.out.println("è stato inserito il numero: " + numbetToAdd);
-        System.out.println(num);
+    public void addElemento(int numberToAdd) {
+        num.add(numberToAdd);
+
+        System.out.println("è stato inserito il numero: " + numberToAdd);
+        System.out.println("La nuova lista: " + num);
+        
+        // aggiorno l'array
+        // System.out.println("debug: num.size(): " + num.size());
+        int[] newArray = new int[num.size()];
+        for(int i = 0; i < num.size(); i++) {
+            newArray[i] = num.get(i);
+        }
+        this.numbers = newArray;
     }
+
+    public void debug() {
+        System.out.println("");
+        System.out.println("-------------------------- debug --------------------------");
+        System.out.println("num length: " + num.size());
+        System.out.println("num content: " + num);
+
+        System.out.println("numbers length: " + numbers.length);
+        for(int i = 0; i < numbers.length; i++)
+            System.out.println("numbers at index " + i + " :" + numbers[i]);
+        }
 
 }
