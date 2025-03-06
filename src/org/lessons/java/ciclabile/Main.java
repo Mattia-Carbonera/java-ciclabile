8b4e585811793a7d782673a036896a1935c34fff
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-06 13:20:22 +0100

    fix output in Main & add secondArray

--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -4,26 +4,44 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        System.out.println("--------------------- fisrtArray ---------------------");
         Number firstArray = new Number(arr);
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
-        firstArray.getNumbers();
+    
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        System.out.println("Elemento corrente: " + firstArray.getNumbers());
+        // System.out.println("Elemento corrente: " + firstArray.getNumbers());
 
-        
-        firstArray.hasAncoraElementi();
+
+        System.out.println("Ci sono ancora elementi? " + (firstArray.hasAncoraElementi() == true ? "Si, ci sono altri elementi" : "No, non ci sono altri elementi"));
 
         firstArray.addElemento(10);
-        firstArray.addElemento(11);
-        firstArray.addElemento(12);
+        System.out.println("");
 
-        firstArray.debug();
+
+        System.out.println("--------------------- fisrtArray ---------------------");
+        Number secondArray = new Number();
+    
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        System.out.println("Elemento corrente: " + secondArray.getNumbers());
+        // System.out.println("Elemento corrente: " + secondArray.getNumbers());
+
+
+        System.out.println("Ci sono ancora elementi? " + (secondArray.hasAncoraElementi() == true ? "Si, ci sono altri elementi" : "No, non ci sono altri elementi"));
+
+        secondArray.addElemento(10);
+        System.out.println("");
 
     }
 }
--- src/org/lessons/java/ciclabile/Number.java
+++ src/org/lessons/java/ciclabile/Number.java
@@ -2,20 +2,42 @@ package org.lessons.java.ciclabile;
 
 public class Number {
     private int[] numbers;
+    private int indexCurrentNumber;
 
     // * costruttore
     public Number(int[] numbers) {
         this.numbers = numbers;
+        this.indexCurrentNumber = 0;
     }
 
-    
-    public void getNumbers() {
+    public Number() {
+        int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        this.numbers = array;
+        this.indexCurrentNumber = 0;
     }
 
-    public void hasAncoraElementi() {
+    // * metodi
+    public int getNumbers() {
+        int currentNum = numbers[indexCurrentNumber];
+        indexCurrentNumber++;
+        return currentNum;
+    }
+
+    public boolean hasAncoraElementi() {
+        return indexCurrentNumber < numbers.length ? true : false;
     }
 
     public void addElemento(int numberToAdd) {
+        int[] newArray = new int[numbers.length + 1];
+        for(int i = 0; i < numbers.length; i++) {
+            newArray[i] = numbers[i];
+        }
+        newArray[numbers.length] = numberToAdd;
+        numbers = newArray;
+        System.out.print("L'elemento '" + numberToAdd + "' è stato aggiunto correttamente, il nuovo array ora ha i seguenti vaolri: ");
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ", ");
+        }
     }
 
     public void debug() {
